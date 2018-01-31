c472eb842805e057086d314b4d197b5678422bd5
peefer66 <peefer66@hotmail.com>
2018-01-31 13:02:07 +0000

    first commit

---
+++ app/routes.py
@@ -0,0 +1,31 @@
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username':'Peefer'}
+    posts = [
+        {
+            'author':{'username':'John'},
+            'body': 'Beautiful day in Portland'
+        },
+        {
+            'author':{'username':'Susan'},
+            'body': 'The Avengers movie was cool'
+        },
+        {
+            'author':{'username':'Trevor'},
+            'body': 'He is a very naughty boy'
+        }
+    ]
+    return render_template('index.html',title='Homes', posts=posts, user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me = {}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
