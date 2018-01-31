c1a70743784ab8e9991015f7059e162e1da1483d
peefer66 <peefer66@hotmail.com>
2018-01-31 15:14:42 +0000

    Mod to rout login check for user and password.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from app.models import User, Post
 
 @app.route('/')
 @app.route('/index')
