2a4f6a5a811f92b9de3ac6ac02ea690eb52a82ad
peefer66 <peefer66@hotmail.com>
2018-02-03 16:48:48 +0000

    added registration form some repair still needed

--- app/forms.py
+++ app/forms.py
@@ -14,7 +14,7 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo(password)])
+    repeat_password = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
     def validate_username(self, username):
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User, Post
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.urls import url_parse
 
 
@@ -10,8 +10,18 @@ from werkzeug.urls import url_parse
 @app.route('/index')
 @login_required
 def index():
-    
-    return render_template('index.html',title='Homes', posts=posts, user=user)
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',title='Homes', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -32,3 +42,22 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Successfuly registered')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)   
+
+
+
+
