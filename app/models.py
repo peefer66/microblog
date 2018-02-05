d195e713e15fb55243f6a3bcf411166b10bd65e1
peefer66 <peefer66@hotmail.com>
2018-02-05 20:00:31 +0000

    Ch 6.2 added avatar to models and user.html

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 
 class User(UserMixin, db.Model):
@@ -21,6 +22,9 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
