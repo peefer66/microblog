f9a18bebec33c265a1f7eeac645a91006e997898
peefer66 <peefer66@hotmail.com>
2018-02-10 16:43:14 +0000

    added followers to models and manto many relationship

--- app/models.py
+++ app/models.py
@@ -5,6 +5,16 @@ from flask_login import UserMixin
 from app import login
 from hashlib import md5
 
+# create an associate table called followers  for the many to many relationship
+# not delclairing this as a model because since it willhave no data other 
+# than the foreign keys
+
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,6 +24,12 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship('User', secondary=followers,
+    primaryjoin=(followers.c.follower_id == id),
+    secondaryjoin=(followers.c.followed_id == id),
+    backref=db.backref('followers', lazy='dynamic'),
+    lazy='dynamic'
+    )
 
     def __repr__(self):
         return '<user {}'.format(self.username)
