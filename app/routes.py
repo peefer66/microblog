c95813dc8b7560ad9650ec76b616d7fc14dbd1bd
peefer66 <peefer66@hotmail.com>
2018-02-12 21:44:23 +0000

    ch 8.8 follow unfollow

--- app/routes.py
+++ app/routes.py
@@ -107,7 +107,7 @@ def follow(username):
         return redirect(url_for('user', username=username))
     current_user.follow(user)
     db.session.commit()
-    flash('You are following {}').format(username)
+    flash('You are following {}'.format(username))
     return redirect(url_for('user', username=username))
 
 
@@ -116,12 +116,12 @@ def follow(username):
 def unfollow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash('User {} not found').format(username)
+        flash('User {} not found'.format(username))
         return redirect(url_for('index'))
     if user == current_user:
         flash('You cannot unfollow yourself')
         return redirect(url_for('user', username=username))
     current_user.unfollow(user)
     db.session.commit()
-    falsh('You unfollowed {}').format(username)
+    flash('You unfollowed {}'.format(username))
     return redirect(url_for('user', username=username))
